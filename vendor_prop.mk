0d31da251db0670db5980214acb7a38657893a19
karthick111 <karthickrko61@gmail.com>
2018-08-21 20:21:14 +0200

    rolex: Add missing sdm props
    * Fixes flickering issues
    
    Change-Id: If12e6e0b85556c4fc5ffcfc18e96161053f30138

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -88,7 +88,9 @@ debug.hwui.use_buffer_age=false \
 persist.graphics.vulkan.disable=true \
 persist.debug.wfd.enable=1 \
 debug.sdm.support_writeback=0 \
-debug.composition.type=gpu
+debug.composition.type=gpu \
+sdm.debug.disable_rotator_split=1 \
+sdm.perf_hint_window=50
 
 # Property to enable display default color mode
 PRODUCT_PROPERTY_OVERRIDES += \
