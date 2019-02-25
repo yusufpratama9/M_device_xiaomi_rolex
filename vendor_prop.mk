6a89b5ac5a413f84cae5f71acf0445d8f426958d
dandyfarid <jason.timber98@gmail.com>
2019-02-25 04:33:31 +0000

    Revert "rolex: vendor_prop: Enable dirac audio effect"
    This reverts commit 6cda7b1b181d89b4dc4b1035e1991e8294041367.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -28,8 +28,7 @@ vendor.voice.conc.fallbackpath=deep-buffer \
 vendor.voice.path.for.pcm.voip=true \
 vendor.audio.use.dts_eagle=true \
 use.dts_eagle=true \
-hpx_send_params=1 \
-persist.audio.dirac.speaker=true
+hpx_send_params=1
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
